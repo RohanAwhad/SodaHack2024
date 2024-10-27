efff5c6c3491fdd4805c955b8df89c507c5a585d
Rohan Awhad <rohanavad007@gmail.com>
2024-10-26 22:45:03 -0700

    added cors

--- backend/api.py
+++ backend/api.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List, Optional
 import main
 
 app = FastAPI(root_path='/api/v1')
+
+# Add CORS middleware to the app
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["*"],  # Allows all origins
+  allow_credentials=True,
+  allow_methods=["*"],  # Allows all methods
+  allow_headers=["*"],  # Allows all headers
+)
 
 # Models
 
